27e34aaf0c45df9494c76527bc56237afda76a59
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2021-10-21 16:05:55 +0200

    Update BasicTutorial.py

--- GettingStarted/BasicTutorial.py
+++ GettingStarted/BasicTutorial.py
@@ -1,1 +1,41 @@
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ ||  Aufgabe 1
+ ||  
+ ||  @brief 
+ ||  @file main.c
+ ||  @author Tjark Ziehm
+ ||  @version 0.9 15.Oktober 2015 
+ ||  @Link http://www.stefan-lenz.ch/_pics/11/ascii.png
+ ||  @Link https://www.tutorialcup.com/cprogramming/c-instructions-in-c-programming.htm
+ || This program is distributed in the hope that it will be useful,
+ || but WITHOUT ANY WARRANTY; without even the implied warranty of
+ || MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ || GNU General Public License for more details.
+ ||
+ || You should have received a copy of the GNU General Public License
+ || along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**************************************************************************/
+/*!    
+    @brief  seperation logical blocks
+    @param  line of single deviding commenting symbol like /,# or /* and */    
+*/
+/**************************************************************************/
